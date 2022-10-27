b25047279e4b7443eecf8d304572998b69002e21
Mstgr <113407111+Mstgr@users.noreply.github.com>
2022-10-27 14:05:48 -0700

    Update printResult.cpp

--- printResult.cpp
+++ printResult.cpp
@@ -9,9 +9,9 @@ std::string printResult(int questions, int correct, int wrong) {
 	std::stringstream ss;
 
 	ss << "Number of questions: " << questions << std::endl;
-	ss << "Number of correct answers: " << correct << std::endl;
+	ss << "Number correct answers: " << correct << std::endl;
 	ss << "Number wrong answers: " << wrong << std::endl;
-	ss << "Final score: " << Quiz::score << std::endl;
+	ss << "Final score: " << Quiz::score;
 
 	return ss.str();
 }
